f85e8cd272066efd3d048ef5cede371df61383c3
Cauê Pereira Cermak <cauecermak@gmail.com>
2022-09-26 14:08:52 +0000

    finalizado

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import forca3 from "./assets/forca3.png";
 import forca4 from "./assets/forca4.png";
 import forca5 from "./assets/forca5.png";
 import forca6 from "./assets/forca6.png";
-import normalizeWindowsPath from "tar/lib/normalize-windows-path";
 
 const array_forca = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 
@@ -36,7 +35,6 @@ function isArraysEqual(word, show_word) {
 function comparador() {
     return Math.random() - 0.5;
 }
-
 
 function change_special_characters(n) {
     let c;
@@ -71,26 +69,25 @@ function change_special_characters(n) {
 }
 
 
-function clickKey(letter, word, show_word, setDisable) {
+function clickKey(letter, word, show_word, setDisable,setErro, erro) {
 
     show_word = word.map((n, i) => (n == letter || show_word[i] === n) ? n : "-");
     show_word == word ? console.log("igual") : console.log("diferente")
-
-    word.includes(letter) ? erro = erro : erro++;
+    
+    word.includes(letter) ? erro = erro : setErro(erro+1);
     erro > 6 ? lost_game = true : lost_game = false;
 
-    //console.log(`word: ${word}`);
-    //console.log("show world: " + show_word);
 
     if (isArraysEqual(word, show_word)) {
         alert("É igual");
         win_game = true;
     }
 
-    if (erro >= 6) {
+    if (erro >= 5) {
         setDisable(true);
         show_word = word;
     }
+    
 
     return show_word;
 
@@ -108,7 +105,7 @@ function randonWord() {
     //return wordPlay.split("");//return string
 }
 
-function wordStyle() {
+function wordStyle(erro) {
 
     if (win_game) {
         return "word green";
@@ -120,10 +117,11 @@ function wordStyle() {
 }
 
 
-function choose_word_bt(disable, setDisable, setWord, setShowWord,setKeyClickedArray) {
+function choose_word_bt(disable, setDisable, setWord, setShowWord,setKeyClickedArray, setErro,setInputValue) {
 
-    let erro = 0;
     let wd;
+    win_game = false;
+    setErro(0);
 
     if (disable) {
         setDisable(false);
@@ -134,21 +132,44 @@ function choose_word_bt(disable, setDisable, setWord, setShowWord,setKeyClickedA
     setShowWord(wd.map((n) => "-"));
     
     setKeyClickedArray([]);
-    
+    setInputValue("");
+}
 
+function inputTry(inputValue,word,end,setErro){
+    
+    //console.log(inputValue);
+    //console.log(word);
+
+    if(isArraysEqual(word, inputValue.split("")) ){
+        win_game = true;//green word
+        end();
+    }else{
+        lost_game = true;
+        setErro(6);
+        end();
+    }
 }
 
 export default function App() {
-    /* const [erro, setErro] = React.useState(0); */
+    const [erro, setErro] = React.useState(0);
     const [keyClickedArray, setKeyClickedArray] = React.useState([]);
     const [disable, setDisable] = React.useState(true);
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     const [word, setWord] = React.useState("");
     const [show_word, setShowWord] = React.useState("");
+    const [inputValue, setInputValue] = React.useState("");
+    
+    function end(){
+        console.log("quantas vezes roda varios sets na mesma função?");
+        setShowWord(word);
+        setDisable(true);
+        /* setErro(6); */
+    }
 
-    function update_forca(word, show_word, letter) {
+    function update_forca(word, show_word, letter,erro) {
+        console.log(erro);
         if (!keyClickedArray.includes(letter)) {
-            setShowWord(clickKey(letter, word, show_word, setDisable));
+            setShowWord(clickKey(letter, word, show_word, setDisable,setErro,erro));
             setKeyClickedArray([...keyClickedArray, letter]);
             console.log(keyClickedArray);
         }
@@ -160,16 +181,16 @@ export default function App() {
             <div className="up">
                 <img src={array_forca[erro]} alt="forca" />
                 <div className="right">
-                    <button onClick={() => choose_word_bt(disable, setDisable, setWord, setShowWord,setKeyClickedArray)}>Escolher Palavra</button>
-                    <span className={wordStyle()} >{word}  {show_word}</span>
+                    <button onClick={() => choose_word_bt(disable, setDisable, setWord, setShowWord,setKeyClickedArray,setErro,setInputValue)}>Escolher Palavra</button>
+                    <span className={wordStyle(erro)} >{word}  {show_word}</span>
                 </div>
             </div>
-
+            
             <div>
                 <div className="keyboard">
                     <div>
                         {alphabet.map((letter, index) =>
-                            <div className={`${disable ? "disable" : ""}  ${keyClickedArray.includes(letter) ? "clicked" : ""}`} key={index} onClick={() => !disable ? update_forca(word, show_word, letter) : ""}>
+                            <div className={`${disable ? "disable" : ""}  ${keyClickedArray.includes(letter) ? "clicked" : ""}`} key={index} onClick={() => !disable ? update_forca(word, show_word, letter,erro) : ""}>
                                 {letter.toLocaleUpperCase()}
                             </div>
                         )}
@@ -177,9 +198,8 @@ export default function App() {
                 </div>
                 <div className={`input_class ${disable ? "disable" : ""}`} >
                     <p >Já sei a palavra!</p>
-                    <input type="text" />
-
-                    <button>CHUTAR</button>
+                    <input type="text" onChange = {e => setInputValue(e.target.value)} value={disable?"":inputValue}/>
+                    <button onClick={()=> inputTry(inputValue,word,end,setErro)}>CHUTAR</button>
                 </div>
             </div>
         </React.Fragment>
